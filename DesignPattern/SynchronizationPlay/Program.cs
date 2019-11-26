5b59f10bba46314b8434f3ab9893e7604b2ff4b0
Sayeed Alam <saalam@microsoft.com>
2019-11-25 16:00:11 -0800

    Added Sync Play

---
+++ DesignPattern/SynchronizationPlay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SynchronizationPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
